8eb9c21f65e1f1ec0f35913bcab1379f29c24d57
Soraya Thorndyke <sthorndyke325@gmail.com>
2019-10-10 18:56:24 -0500

    started loan calculator

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html', display="", pageTitle='Loan Payment Calculator')
+@app.route('/add', methods=['GET', 'post'])
+def addition():
+    if request.method =='post':
+        form = request.form
+        A = int(form['A'])
+        D = int(form['D'])
+        calc = A + D
+        return render_template('index.html', display=calc, pageTitle='Loan Payment Calculator')
 
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)

4c85e836a85cfdbe217ca7bb7bbe24c9f885134f
Soraya Thorndyke <sthorndyke325@gmail.com>
2019-10-09 19:10:55 -0500

    modified base, index, and style

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Flask Server Home Page')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

6873478694e8d7b5a1dfb371b40045fe3c28e150
Soraya Thorndyke <sthorndyke325@gmail.com>
2019-10-11 13:20:11 -0500

    added more calculations

--- app.py
+++ app.py
@@ -7,14 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', display="", pageTitle='Loan Payment Calculator')
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/Divide', methods=['GET', 'POST'])
 def Divide():
     if request.method =='POST':
         form = request.form
-        A = int(form['A'])
-        N = int(form['N'])
-        i = int(form['i'])
-        calc =  A/ ((( 1 + i ) ^n ) - 1 ) / ( i ( 1+ i) ^n)
+        A = float(form['NumOne'])
+        n = float(form['NumTwo'])
+        i = float(form['NumThree'])
+        D = float(form['NumFour'])
+        n = years * monthly payments
+        i = annual rate / monthly payments
+        d = ((( 1 + i ) ^^n ) - 1 ) / ( i ( 1+ i) ^^n)
+        calc =  A/D
+        calc = round(number[, 2])
         return render_template('index.html', display=calc, pageTitle='Loan Payment Calculator')
 
     return redirect("/")
